13a60f7c12e992d9d7082457dfcfc56215fe77a4
Reuben Ramos <rsramos23@gmail.com>
2024-01-17 17:08:48 -0800

    finished string

--- js/strings.js
+++ js/strings.js
@@ -1,19 +1,44 @@
 //STEP 1
+// let name  = prompt('Enter your name')
 
+// let length = name.length
+// alert(`Your name is: ${length} characters long`)
 //STEP 2
+// let user = prompt('Enter your name')
+// let num = prompt('Enter a Number to return the letter within the name')
+// let index = user.charAt(num)
+// alert(`The number ${num} returns the "${index}" character`)
 
 //STEP 3
-
+// let firstName = prompt('Enter your first name')
+// let lastName = prompt('Enter your last name')
+// alert('Your name is:' + firstName.concat(" ", lastName))
 //STEP 4
-
+// let message  = 'The quick brown fox jumps over the lazy dog'
+// alert("'fox' is at index:" + message.indexOf("fox"))
 //STEP 5
-
+// let message  = 'The quick brown fox jumps over the lazy dog'
+// alert("The last instance of the word 'fox' is at index:" + message.lastIndexOf('fox'))
 //STEP 6
-
+// let message  = 'The quick brown fox jumps over the lazy dog'
+// let name = prompt('Enter your first and last name')
+// alert(message.replace('the lazy dog', name))
 //STEP 7
-
+// let message  = 'The quick brown fox jumps over the lazy dog'
+// let word = prompt('Enter a word')
+// let index = message.search(word)
+// if (index == -1){
+// alert('Word not found')
+// }else{
+//     alert(`Word found at index ${index}`)
+// }
 //STEP 8
-
+// let old_string  = 'The quick brown fox jumps over the lazy dog'
+// let new_string = old_string.substring(31)
+// alert(new_string.toUpperCase())
 //STEP 9
-
+// let message  = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG'
+// alert("' " + message.trim().toLowerCase() + "  '")
 //STEP 10
+// let message  = 'the quick brown fox jumps over the lazy dog'
+// alert(message.charAt(0).toUpperCase() + message.slice(1))
