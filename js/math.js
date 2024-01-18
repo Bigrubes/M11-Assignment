91e820e06105565d313df555a9b9921b1e18360a
Reuben Ramos <rsramos23@gmail.com>
2024-01-17 17:33:36 -0800

    math completed

--- js/math.js
+++ js/math.js
@@ -1,9 +1,19 @@
 //STEP 1
-
+// let num = prompt('Enter a positive or negative number')
+// console.log(Math.abs(num))
 //STEP 2
+// let num = prompt('Enter a decimal number')
+// console.log(Math.ceil(num))
 
 //STEP 3
-
+// let num = prompt('Enter a decimal number')
+// console.log(Math.floor(num))
 //STEP 4
-
+// let numbers = prompt('Enter 5 numbers and separate each with a comma')
+// let num  = numbers.split(',')
+// let bigNum = Math.max(...num)
+// let smallNum = Math.min(...num)
+// console.log(`Smallest number: ${smallNum}` + ` and Biggest number: ${bigNum}`)
 //STEP 5
+// let num = prompt('Enter a number to find square root')
+// console.log(Math.sqrt(num))
