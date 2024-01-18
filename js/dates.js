245852db4e3f1c877d6c5e2f9ad100ce353ce380
Reuben Ramos <rsramos23@gmail.com>
2024-01-17 18:16:23 -0800

    dates completed

--- js/dates.js
+++ js/dates.js
@@ -1,9 +1,26 @@
 //STEP 1
-
+// let month = prompt('Enter a number month')
+// let year = prompt('Enter a year')
+// const days = function (month, year) {
+//     return new Date(year, month, 0).getDate()
+// }
+// console.log(`${days(month,year)} days in month selected`)
 //STEP 2
-
+// let date  = new Date(prompt('Enter a date: (yyyy-mm-dd)'))
+// const month = date.toLocaleString('default', {month: 'long'})
+// console.log(month)
 //STEP 3
-
+// let date  = new Date(prompt('Enter a date: (yyyy-mm-dd)'))
+// const day = date.getDay()
+// if (day == 6 || day == 0){
+//     console.log(`${date} is a weekend day`)
+// }else{
+//     console.log(`${date} is not a weekend day`)}
 //STEP 4
-
+// let yesterday = new Date()
+// yesterday.setDate(yesterday.getDate() -1)
+// console.log(yesterday.toDateString())
 //STEP 5
+// let today = new Date()
+// let text = today.toString()
+// console.log(text.slice(0,1))
